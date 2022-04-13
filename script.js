621580d24f58f082b9ba407dffe323ac87474fbe
John Blox <103267235+JohnXander@users.noreply.github.com>
2022-04-13 22:46:07 +0000

    CSS completed

--- script.js
+++ script.js
@@ -17,21 +17,21 @@ function createUserCard(user) {
     const cardHTML = `
 
         <div class="card">
-            <div>
+            <div class="img-container">
                 <img
                     class="avatar"
                     src="${user.avatar_url}"
                     alt="${user.name}"
                 />
             </div>
-            <div>
+            <div class="user-info">
                 <h2>${user.name}</h2>
                 <p>${user.bio}</p>
 
                 <ul class="info">
-                    <li>${user.followers}</li>
-                    <li>${user.following}</li>
-                    <li>${user.public_repos}</li>
+                    <li>${user.followers}<strong>Followers</strong></li>
+                    <li>${user.following}<strong>Following</strong></li>
+                    <li>${user.public_repos}<strong>Repos</strong></li>
                 </ul>
             </div>
         </div>
